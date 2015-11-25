8cc09b4fc63b7e3f2530277eb2757df3c512cc03
encse <encse@csokavar.hu>
2015-11-25 23:32:36 +0100

    remove angular, add ace editor

--- Sicp/app.js
+++
@@ -1,4 +0,0 @@
-window.onload = function () {
-    new Parser().parse('(allmda () "sss" )');
-};
-//# sourceMappingURL=app.js.map
--- Sicp/main.js
+++ Sicp/main.js
@@ -1,152 +1,34 @@
-var TodoCtrl = (function () {
-    function TodoCtrl($scope) {
+var SicpEditor = (function () {
+    function SicpEditor(editorId, outputId) {
         var _this = this;
-        this.scope = $scope;
-        this.scope.name = "(define fact\n    (lambda(x)\n        (cond\n            ((= x 1) 1)\n            (else (* x (fact (- x 1))))))))\n(fact 5)\n(fact 4)\n";
-        this.scope.clickMe = function (event) {
-            if (event.shiftKey && event.keyCode === 13) {
-                try {
-                    _this.scope.clickEvent = new Scheme.Evaluator().evaluateString(_this.scope.name);
+        this.editorId = editorId;
+        this.outputId = outputId;
+        require(['ace/ace'], function (ace) {
+            _this.editor = ace.edit("editor");
+            _this.editor.setTheme('ace/theme/monokai');
+            _this.editor.getSession().setMode('ace/mode/sicp');
+            _this.editor.commands.addCommand({
+                name: 'Evaluate',
+                bindKey: { win: 'Ctrl-E', mac: 'Command-E' },
+                exec: function (editor) {
+                    try {
+                        _this.setOutput(new Scheme.Evaluator().evaluateString(editor.getValue()));
+                    }
+                    catch (ex) {
+                        _this.setOutput(ex);
+                    }
                 }
-                catch (event) {
-                    _this.scope.clickEvent = event;
-                }
-                event.preventDefault();
-            }
-            else {
-                _this.foo(event.srcElement, event);
-            }
-        };
+            });
+            _this.outputElement = document.getElementById(outputId);
+        });
     }
-    TodoCtrl.prototype.isMultiLine = function (el) {
-        // Extract the selection
-        var snippet = el.value.slice(el.selectionStart, el.selectionEnd);
-        var nlRegex = /\n/;
-        if (nlRegex.test(snippet))
-            return true;
-        else
-            return false;
+    SicpEditor.prototype.setOutput = function (st) {
+        this.outputElement.innerText = st;
     };
-    TodoCtrl.prototype.findStartIndices = function (el) {
-        var text = el.value, startIndices = [], offset = 0;
-        while (text.match(/\n/) && text.match(/\n/).length > 0) {
-            offset = (startIndices.length > 0 ? startIndices[startIndices.length - 1] : 0);
-            var lineEnd = text.search("\n");
-            startIndices.push(lineEnd + offset + 1);
-            text = text.substring(lineEnd + 1);
-        }
-        startIndices.unshift(0);
-        return startIndices;
-    };
-    TodoCtrl.prototype.foo = function (element, event) {
-        var tab = '    ';
-        var tabWidth = tab.length;
-        if (event.keyCode === 9) {
-            event.preventDefault();
-            var currentStart = element.selectionStart, currentEnd = element.selectionEnd;
-            if (event.shiftKey === false) {
-                // Normal Tab Behaviour
-                if (!this.isMultiLine(element)) {
-                    // Add tab before selection, maintain highlighted text selection
-                    element.value = element.value.slice(0, currentStart) + tab + element.value.slice(currentStart);
-                    element.selectionStart = currentStart + tabWidth;
-                    element.selectionEnd = currentEnd + tabWidth;
-                }
-                else {
-                    // Iterating through the startIndices, if the index falls within selectionStart and selectionEnd, indent it there.
-                    var startIndices = this.findStartIndices(element), l = startIndices.length, newStart = undefined, newEnd = undefined, affectedRows = 0;
-                    while (l--) {
-                        var lowerBound = startIndices[l];
-                        if (startIndices[l + 1] && currentStart != startIndices[l + 1])
-                            lowerBound = startIndices[l + 1];
-                        if (lowerBound >= currentStart && startIndices[l] < currentEnd) {
-                            element.value = element.value.slice(0, startIndices[l]) + tab + element.value.slice(startIndices[l]);
-                            newStart = startIndices[l];
-                            if (!newEnd)
-                                newEnd = (startIndices[l + 1] ? startIndices[l + 1] - 1 : 'end');
-                            affectedRows++;
-                        }
-                    }
-                    element.selectionStart = newStart;
-                    element.selectionEnd = (newEnd !== 'end' ? newEnd + (tabWidth * affectedRows) : element.value.length);
-                }
-            }
-            else {
-                // Shift-Tab Behaviour
-                if (!this.isMultiLine(element)) {
-                    if (element.value.substr(currentStart - tabWidth, tabWidth) == tab) {
-                        // If there's a tab before the selectionStart, remove it
-                        element.value = element.value.substr(0, currentStart - tabWidth) + element.value.substr(currentStart);
-                        element.selectionStart = currentStart - tabWidth;
-                        element.selectionEnd = currentEnd - tabWidth;
-                    }
-                    else if (element.value.substr(currentStart - 1, 1) == "\n" && element.value.substr(currentStart, tabWidth) == tab) {
-                        // However, if the selection is at the start of the line, and the first character is a tab, remove it
-                        element.value = element.value.substring(0, currentStart) + element.value.substr(currentStart + tabWidth);
-                        element.selectionStart = currentStart;
-                        element.selectionEnd = currentEnd - tabWidth;
-                    }
-                }
-                else {
-                    // Iterating through the startIndices, if the index falls within selectionStart and selectionEnd, remove an indent from that row
-                    var startIndices = this.findStartIndices(element), l = startIndices.length, newStart = undefined, newEnd = undefined, affectedRows = 0;
-                    while (l--) {
-                        var lowerBound = startIndices[l];
-                        if (startIndices[l + 1] && currentStart != startIndices[l + 1])
-                            lowerBound = startIndices[l + 1];
-                        if (lowerBound >= currentStart && startIndices[l] < currentEnd) {
-                            if (element.value.substr(startIndices[l], tabWidth) == tab) {
-                                // Remove a tab
-                                element.value = element.value.slice(0, startIndices[l]) + element.value.slice(startIndices[l] + tabWidth);
-                                affectedRows++;
-                            }
-                            else {
-                            } // Do nothing
-                            newStart = startIndices[l];
-                            if (!newEnd)
-                                newEnd = (startIndices[l + 1] ? startIndices[l + 1] - 1 : 'end');
-                        }
-                    }
-                    element.selectionStart = newStart;
-                    element.selectionEnd = (newEnd !== 'end' ? newEnd - (affectedRows * tabWidth) : element.value.length);
-                }
-            }
-        }
-        else if (event.keyCode === 13 && event.shiftKey === false) {
-            var self = this, cursorPos = element.selectionStart, startIndices = self.findStartIndices(element), numStartIndices = startIndices.length, startIndex = 0, endIndex = 0, tabMatch = new RegExp("^" + tab.replace('\t', '\\t').replace(/ /g, '\\s') + "+", 'g'), lineText = '';
-            var tabs = null;
-            for (var x = 0; x < numStartIndices; x++) {
-                if (startIndices[x + 1] && (cursorPos >= startIndices[x]) && (cursorPos < startIndices[x + 1])) {
-                    startIndex = startIndices[x];
-                    endIndex = startIndices[x + 1] - 1;
-                    break;
-                }
-                else {
-                    startIndex = startIndices[numStartIndices - 1];
-                    endIndex = element.value.length;
-                }
-            }
-            lineText = element.value.slice(startIndex, endIndex);
-            tabs = lineText.match(tabMatch);
-            if (tabs !== null) {
-                event.preventDefault();
-                var indentText = tabs[0];
-                var indentWidth = indentText.length;
-                var inLinePos = cursorPos - startIndex;
-                if (indentWidth > inLinePos) {
-                    indentWidth = inLinePos;
-                    indentText = indentText.slice(0, inLinePos);
-                }
-                element.value = element.value.slice(0, cursorPos) + "\n" + indentText + element.value.slice(cursorPos);
-                element.selectionStart = cursorPos + indentWidth + 1;
-                element.selectionEnd = element.selectionStart;
-            }
-        }
-    };
-    return TodoCtrl;
+    return SicpEditor;
 })();
-var todomvc = angular.module('sicp', [])
-    .controller('TodoCtrl', TodoCtrl);
+///<reference path="./sicpEditor.ts"/>
+new SicpEditor("editor", "output");
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
@@ -732,4 +614,3 @@ var Scheme;
         return Token;
     })();
 })(Scheme || (Scheme = {}));
-//# sourceMappingURL=main.js.map
