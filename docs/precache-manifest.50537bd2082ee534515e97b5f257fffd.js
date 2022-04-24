568ab8a08daf17146c1f8cbe9642985c37e39f16
David Nemeth Csoka <encse@csokavar.hu>
2022-04-24 07:34:56 +0200

    x

---
+++ docs/precache-manifest.50537bd2082ee534515e97b5f257fffd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b6d69fc2a60177b48d604bbc085a50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1604c062a760ab33e31d",
+    "url": "/static/css/main.eca3414f.chunk.css"
+  },
+  {
+    "revision": "9e38b8f288b7eda4a4f0",
+    "url": "/static/js/2.ac266721.chunk.js"
+  },
+  {
+    "revision": "1604c062a760ab33e31d",
+    "url": "/static/js/main.4c7435f3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.78ea88d73fe5c3f63334e1ea8a088e2f.js"
+  "/precache-manifest.50537bd2082ee534515e97b5f257fffd.js"
 );
 
 self.addEventListener('message', (event) => {
